f1f6800b1a905c18b5a6d4febfa113dd8ccc8592
tomlic <charles.tomlin@ons.gov.uk>
2023-03-20 19:35:37 +0000

    Added stage 2 matchkeys and updated stage 1 matchkeys (testing only)

---
+++ matchkeys/Stage_1/main_matchkeys.py
@@ -0,0 +1,22 @@
+def run_matchkeys(cen, pes, level):
+    import pandas as pd
+    from library.matching import run_single_matchkey
+
+    "Stage 1 Matchkey list"
+    mk1 = run_single_matchkey(df1=cen, df2=pes, suffix_1='_cen', suffix_2='_pes', hh_id='hid', level=level, matchkey=1, variables=['forename_clean', 'last_name_clean', 'full_dob'])
+    mk2 = run_single_matchkey(df1=cen, df2=pes, suffix_1='_cen', suffix_2='_pes', hh_id='hid', level=level, matchkey=2, variables=['telephone', 'full_dob'])
+    matchkey_list = [mk1, mk2]
+
+    df = pd.DataFrame()
+    
+    for i, matches in enumerate(matchkey_list):
+
+        matches['MK'] = i + 1
+    
+        df = pd.concat([df, matches], axis=0)
+    
+        df['Min_MK'] = df.groupby(['puid_cen', 'puid_pes'])['MK'].transform('min')
+    
+        df = df[df.Min_MK == df.MK].drop(['Min_MK'], axis=1)    
+    
+    return df
