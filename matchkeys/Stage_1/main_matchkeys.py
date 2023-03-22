5aa57237dcd0e4cd431e18f358c2bfd93c415bce
tomlic <charles.tomlin@ons.gov.uk>
2023-03-22 09:43:26 +0000

    Used pylint and black to tidy matchkey function scripts

--- matchkeys/Stage_1/main_matchkeys.py
+++ matchkeys/Stage_1/main_matchkeys.py
@@ -1,22 +1,20 @@
-def run_matchkeys(cen, pes, level):
-    import pandas as pd
-    from library.matching import run_single_matchkey
+import pandas as pd
+from library.matching import run_single_matchkey
 
-    "Stage 1 Matchkey list"
-    mk1 = run_single_matchkey(df1=cen, df2=pes, suffix_1='_cen', suffix_2='_pes', hh_id='hid', level=level, matchkey=1, variables=['forename_clean', 'last_name_clean', 'full_dob'])
-    mk2 = run_single_matchkey(df1=cen, df2=pes, suffix_1='_cen', suffix_2='_pes', hh_id='hid', level=level, matchkey=2, variables=['telephone', 'full_dob'])
+
+def run_matchkeys(cen, pes, level):
+    """Function to define and run Stage 1 Main Matchkeys"""
+
+    mk1 = run_single_matchkey(df1=cen, df2=pes, suffix_1="_cen", suffix_2="_pes", hh_id="hid", level=level, matchkey=1, variables=["forename_clean", "last_name_clean", "full_dob"])
+    mk2 = run_single_matchkey(df1=cen, df2=pes, suffix_1="_cen", suffix_2="_pes", hh_id="hid", level=level, matchkey=2, variables=["telephone", "full_dob"])
+
     matchkey_list = [mk1, mk2]
 
     df = pd.DataFrame()
-    
-    for i, matches in enumerate(matchkey_list):
 
-        matches['MK'] = i + 1
-    
+    for i, matches in enumerate(matchkey_list):
+        matches["MK"] = i + 1
         df = pd.concat([df, matches], axis=0)
-    
-        df['Min_MK'] = df.groupby(['puid_cen', 'puid_pes'])['MK'].transform('min')
-    
-        df = df[df.Min_MK == df.MK].drop(['Min_MK'], axis=1)    
-    
+        df["Min_MK"] = df.groupby(["puid_cen", "puid_pes"])["MK"].transform("min")
+        df = df[df.Min_MK == df.MK].drop(["Min_MK"], axis=1)
     return df
--- matchkeys/Stage_2/associative_matchkeys.py
+++ matchkeys/Stage_2/associative_matchkeys.py
@@ -1,20 +1,19 @@
-def run_matchkeys(cen, pes, level):
-    import pandas as pd
-    from library.matching import run_single_matchkey
+import pandas as pd
+from library.matching import run_single_matchkey
 
-    "Stage 1 Associative list"
-    mk1 = run_single_matchkey(df1=cen, df2=pes, suffix_1='_cen', suffix_2='_pes', hh_id='hid', level=level, matchkey=1, variables=['forename_tri', 'year'])
+
+def run_matchkeys(cen, pes, level):
+    """Function to define and run Stage 2 Associative Matchkeys"""
+
+    mk1 = run_single_matchkey(df1=cen, df2=pes, suffix_1="_cen", suffix_2="_pes", hh_id="hid", level=level, matchkey=1, variables=["forename_tri", "year"])
+
     matchkey_list = [mk1]
 
     df = pd.DataFrame()
 
     for i, matches in enumerate(matchkey_list):
-        matches['MK'] = i + 1
-
+        matches["MK"] = i + 1
         df = pd.concat([df, matches], axis=0)
-
-        df['Min_MK'] = df.groupby(['puid_cen', 'puid_pes'])['MK'].transform('min')
-
-        df = df[df.Min_MK == df.MK].drop(['Min_MK'], axis=1)
-
+        df["Min_MK"] = df.groupby(["puid_cen", "puid_pes"])["MK"].transform("min")
+        df = df[df.Min_MK == df.MK].drop(["Min_MK"], axis=1)
     return df
