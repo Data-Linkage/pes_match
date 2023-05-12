ff105d6bf1cf8288566771a8be760e9c1b5905af
Lynch <Joshua.Lynch@ext.ons.gov.uk>
2023-05-12 18:01:20 +0100

    fix: ammend conf.py to match ons

--- docs/conf.py
+++ docs/conf.py
@@ -12,6 +12,7 @@
 #
 import os
 import sys
+import subprocess
 
 sys.path.insert(0, os.path.abspath("."))
 sys.path.insert(0, os.path.abspath(".."))
@@ -46,15 +47,46 @@ templates_path = ["_templates"]
 # This pattern also affects html_static_path and html_extra_path.
 exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
 
+# -- Options for HTML output -----------------------------------------------------------
 
-# -- Options for HTML output -------------------------------------------------
+# The theme to use for HTML and HTML Help pages.  See the documentation for a list of
+# builtin themes.
+html_theme = "govuk_tech_docs_sphinx_theme"
 
-# The theme to use for HTML and HTML Help pages.  See the documentation for
-# a list of builtin themes.
-#
-html_theme = "alabaster"
+# Variables to pass to each HTML page to help populate page-specific options
+html_context = {
+    "github_url": "https://www.github.com/best-practice-and-impact/govcookiecutter",
+    "gitlab_url": None,
+    "conf_py_path": "docs/",
+    "version": "main",
+    "accessibility": "accessibility.md",
+}
 
-# Add any paths that contain custom static files (such as style sheets) here,
-# relative to this directory. They are copied after the builtin static files,
-# so a file named "default.css" will overwrite the builtin "default.css".
+# Theme options are theme-specific and customize the look and feel of a theme further.
+# For a list of options available for each theme, see the documentation.
+html_theme_options = {"organisation": "ONS", "phase": "Alpha"}
+
+# Add any paths that contain custom static files (such as style sheets) here, relative
+# to this directory. They are copied after the builtin static files, so a file named
+# "default.css" will overwrite the builtin "default.css".
 html_static_path = ["_static"]
+
+# Output file base name for HTML help builder.
+htmlhelp_basename = "pes_matchdoc"
+
+# -- Options for govuk-tech-docs-sphinx-theme ------------------------------------------
+
+# Get the latest Git commit hash — this is used to redirect the 'View Source' link
+# correctly. If this fails, default to `main`. Based on code snippet from:
+# https://github.com/sphinx-doc/sphinx/blob/1ebc9c26c7a4c484733beb9f8e39e93846d86494/sphinx/__init__.py#L53  # noqa: E501
+try:
+    p = subprocess.run(
+        ["git", "show", "-s", "--pretty=format:%H"],
+        stdout=subprocess.PIPE,
+        stderr=subprocess.PIPE,
+        encoding="ascii",
+    )
+    git_version = p.stdout if p.stdout else "main"
+except Exception:
+    git_version = "main"
+    
