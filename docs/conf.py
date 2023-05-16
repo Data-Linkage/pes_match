43d030f5901cecb16f6a62799cacc507c81ccea4
Lynch <Joshua.Lynch@ext.ons.gov.uk>
2023-05-16 15:16:00 +0100

    add: add conf.py extension for sphinx to work with ghpages

--- docs/conf.py
+++ docs/conf.py
@@ -12,15 +12,16 @@
 #
 import os
 import sys
-sys.path.insert(0, os.path.abspath('.'))
-sys.path.insert(0, os.path.abspath('..'))
+
+sys.path.insert(0, os.path.abspath("."))
+sys.path.insert(0, os.path.abspath(".."))
 
 
 # -- Project information -----------------------------------------------------
 
-project = 'PES MATCH'
-copyright = '2023, Charlie Tomlin'
-author = 'Charlie Tomlin'
+project = "PES MATCH"
+copyright = "2023, Charlie Tomlin"
+author = "Charlie Tomlin"
 
 
 # -- General configuration ---------------------------------------------------
@@ -29,23 +30,24 @@ author = 'Charlie Tomlin'
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = [
-    'sphinx.ext.napoleon',      # Supports Google / Numpy docstring
-    'sphinx.ext.autodoc',       # Documentation from docstrings
-    'sphinx.ext.doctest',       # Test snippets in documentation
-    'sphinx.ext.todo',          # to-do syntax highlighting
-    'sphinx.ext.ifconfig',      # Content based configuration
-    'm2r2'                      # Markdown support
+    "sphinx.ext.napoleon",  # Supports Google / Numpy docstring
+    "sphinx.ext.autodoc",  # Documentation from docstrings
+    "sphinx.ext.doctest",  # Test snippets in documentation
+    "sphinx.ext.todo",  # to-do syntax highlighting
+    "sphinx.ext.ifconfig",  # Content based configuration
+    "sphinx.ext.githubpages",
+    "m2r2",  # Markdown support
 ]
 
 source_suffix = [".rst", ".md"]
 
 # Add any paths that contain templates here, relative to this directory.
-templates_path = ['_templates']
+templates_path = ["_templates"]
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
 # This pattern also affects html_static_path and html_extra_path.
-exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
 
 
 # -- Options for HTML output -------------------------------------------------
@@ -53,9 +55,9 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'alabaster'
+html_theme = "alabaster"
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ['_static']
+html_static_path = ["_static"]
